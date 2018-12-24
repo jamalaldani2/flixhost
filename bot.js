50ccbc5584aa3937a621e341f8ad8bee09c60d73
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-24 15:04:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -370,6 +370,45 @@ const Embed11 = new Discord.RichEmbed()
 m3a4x.channel.sendEmbed(Embed11).then(m3a4x => {m3a4x.delete(20000)})
     }
 })
+client.on("message", msg => {
+    if(msg.author.bot) return;
+if(msg.channel.type === 'dm') return;
+
+let p = "$";
+let msgarray = msg.content.split(" ");
+let cmd = msgarray[0];
+let args = msgarray.slice(1);
+
+
+
+if(cmd === `${p}kick`){
+
+
+    let kUser = msg.guild.member(msg.mentions.users.first() || msg.guild.members.get(args[0]));
+if(!kUser) return msg.channel.send("Can't find user!");
+let kreason = args.join(" ").slice(22);
+if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send("No can do pal!");
+if(kUser.hasPermission("KICK_MEMBERS")) return msg.channel.send("That person can't be kicked");
+
+let kickembed = new Discord.RichEmbed()
+.setDescription("~kick~")
+.setColor("BLACK")
+.addField("Kiced User", `${kUser} with ID: ${kUser.id}`)
+.addField("Kicked By", `<@${msg.author.id}> with ID: ${msg.author.id}`)
+.addField("Kicked In", msg.channel)
+.addField("Time", msg.createdAt)
+.addField("Reason", kreason)
+
+let kickChannel = msg.guild.channels.find(`name`,"اسم الروم");
+if(!kickChannel) return msg.channel.send("Can't find `اسم الروم` channel.");
+
+msg.guild.member(kUser).kick(kreason);
+kickChannel.send(kickembed)
+    return;
+
+}
+
+});
 
 
 
